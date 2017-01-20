8ce3bea63990c0ed192e6344880e39b9620a38dd
Michael Yatco <mjyatco@gmail.com>
2017-01-19 21:33:00 -0500

    I've added a get/post route and an index route

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,4 +10,14 @@ class ContactsController < ApplicationController
    render "allcontacts.html.erb"
   end
   
+  def new_contact_method
+    render "new_contact_form.html.erb"
+  end
+
+  def new_contact_result_method
+    @contact = Contact.new(first_name: params[:input_first_name], last_name: params[:input_last_name], email: params[:input_email], phone_number: params[:input_phone_number])
+    @contact.save
+    render "new_contact_result_method.html.erb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/contacts", to: "contacts#first_contact_method"
   get "/allcontacts", to: "contacts#all_contact_method"
+
+  get "/new_contact", to: "contacts#new_contact_method"
+  post "/new_contact_result", to: "contacts#new_contact_result_method"
 end
