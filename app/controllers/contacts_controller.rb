acf5b2afff34d994cf0a33cefbce0b8fe3c6b805
Michael Yatco <mjyatco@gmail.com>
2017-01-23 20:26:57 -0500

    Updated contacts app to make it more RESTful

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,59 @@
 class ContactsController < ApplicationController
 
-  def first_contact_method
-    @contact = Contact.first
-    render "contacts.html.erb"
-  end
+  # def first_contact_method
+  #   @contact = Contact.first
+  #   render "contacts.html.erb"
+  # end
 
-  def all_contact_method
-   @contacts = Contact.all
-   render "allcontacts.html.erb"
-  end
+  # def all_contact_method
+  #  @contacts = Contact.all
+  #  render "allcontacts.html.erb"
+  # end
   
-  def new_contact_method
-    render "new_contact_form.html.erb"
+  # def new_contact_method
+  #   render "new_contact_form.html.erb"
+  # end
+
+  # def new_contact_result_method
+  #   @contact = Contact.new(first_name: params[:input_first_name], last_name: params[:input_last_name], email: params[:input_email], phone_number: params[:input_phone_number])
+  #   @contact.save
+  #   render "new_contact_result_method.html.erb"
+  # end
+  def index
+    @contacts = Contact.all
+    render "index.html.erb"
   end
 
-  def new_contact_result_method
-    @contact = Contact.new(first_name: params[:input_first_name], last_name: params[:input_last_name], email: params[:input_email], phone_number: params[:input_phone_number])
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @contact = Contact.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+    render "create.html.erb"
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render "show.html.erb"
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.assign_attributes(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
     @contact.save
-    render "new_contact_result_method.html.erb"
+    render "update.html.erb"
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render "destroy.html.erb"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/contacts", to: "contacts#first_contact_method"
-  get "/allcontacts", to: "contacts#all_contact_method"
+  # get "/contacts", to: "contacts#first_contact_method"
+  # get "/allcontacts", to: "contacts#all_contact_method"
 
-  get "/new_contact", to: "contacts#new_contact_method"
-  post "/new_contact_result", to: "contacts#new_contact_result_method"
+  # get "/new_contact", to: "contacts#new_contact_method"
+  # post "/new_contact_result", to: "contacts#new_contact_result_method"
+  root to: "contacts#index"
+
+  get "/contacts", to: "contacts#index"
+  get "/contacts/new", to: "contacts#new"
+  post "/contacts", to: "contacts#create"
+  get "/contacts/:id", to: "contacts#show"
+  get "contacts/:id/edit", to: "contacts#edit"
+  patch "/contacts/:id", to: "contacts#update"
+  delete "/contacts/:id", to: "contacts#destroy"
 end
